fb0ee342e5d2dba70af40b4710d798979d7f9efd
FrancescoGM <Francescogmuller@hotmail.com>
2020-07-12 21:02:58 -0300

    add input mask's of type money,cnpj,cep and cellphone

---
+++ public/scripts/input_masks.js
@@ -0,0 +1,48 @@
+function addEventListenerForEach(ElementIndicator, callback) {
+    document.querySelectorAll(ElementIndicator).forEach(element => {
+        element.addEventListener('input', callback)
+    })
+}
+
+function maskCellphone(event) {
+    event.target.value = event.target.value
+        .replace(/\D/g, '')
+        .replace(/(\d{0})(\d{2})/, '$1($2) ')
+        .replace(/(\d{5})(\d{1,4})/, '$1-$2')
+        .replace(/(-\d{4})\d+?$/, '$1')
+}
+
+function maskCnpj(event) {
+    event.target.value = event.target.value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1/$2')
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
+}
+function maskCep(event) {
+    event.target.value = event.target.value
+        .replace(/\D/g, '')
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        .replace(/(-\d{3})\d+?$/, '$1')
+}
+function maskMoney(event) {
+
+    event.target.value = event.target.value
+        .replace(/\D/g, '')
+        .replace(/(\d)/, 'R$ $1')
+        .replace(/(\d{20})\d+?$/, '$1')
+        .replace(/(\d)(\d{17}$)/, '$1.$2')
+        .replace(/(\d)(\d{14}$)/, '$1.$2')
+        .replace(/(\d)(\d{11}$)/, '$1.$2')
+        .replace(/(\d)(\d{8}$)/, '$1.$2')
+        .replace(/(\d)(\d{5}$)/, '$1.$2')
+        .replace(/(\d)(\d{2}$)/, '$1,$2')
+
+}
+
+addEventListenerForEach('.telefone', maskCellphone)
+addEventListenerForEach('.cnpj', maskCnpj)
+addEventListenerForEach('.cep', maskCep)
+addEventListenerForEach('.dinheiro', maskMoney)
