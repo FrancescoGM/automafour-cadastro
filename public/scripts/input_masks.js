e917eea94877e147655c26c741d0b379c060001d
FrancescoGM <Francescogmuller@hotmail.com>
2020-07-12 22:22:58 -0300

    refactoring mask's

--- public/scripts/input_masks.js
+++ public/scripts/input_masks.js
@@ -10,7 +10,7 @@ function maskCellphone(event) {
         .replace(/(\d{0})(\d{1,2})/, '$1($2')
         .replace(/(\d{2})(\d)/, '$1) $2')
         .replace(/(\d{5})(\d{1,4})/, '$1-$2')
-        .replace(/(-\d{4})\d+?$/, '$1')
+        .replace(/(-\d{4})\d$/, '$1')
 }
 
 function maskCnpj(event) {
@@ -20,20 +20,20 @@ function maskCnpj(event) {
         .replace(/(\d{3})(\d)/, '$1.$2')
         .replace(/(\d{3})(\d)/, '$1/$2')
         .replace(/(\d{4})(\d)/, '$1-$2')
-        .replace(/(-\d{2})\d+?$/, '$1')
+        .replace(/(-\d{2})\d$/, '$1')
 }
 function maskCep(event) {
     event.target.value = event.target.value
         .replace(/\D/g, '')
         .replace(/(\d{5})(\d)/, '$1-$2')
-        .replace(/(-\d{3})\d+?$/, '$1')
+        .replace(/(-\d{3})\d$/, '$1')
 }
 function maskMoney(event) {
 
     event.target.value = event.target.value
         .replace(/\D/g, '')
         .replace(/(\d)/, 'R$ $1')
-        .replace(/(\d{20})\d+?$/, '$1')
+        .replace(/(\d{20})\d$/, '$1')
         .replace(/(\d)(\d{17}$)/, '$1.$2')
         .replace(/(\d)(\d{14}$)/, '$1.$2')
         .replace(/(\d)(\d{11}$)/, '$1.$2')
